6d40cdfb0793ea1ffadd173da35b50a2f95c4075
hyisu <143268689+hyisu@users.noreply.github.com>
2023-11-27 14:06:09 +0900

    [Add]JS_Array

---
+++ Freecamp_Frontend_Study/class/03-javascript/연습-03-object-array.js
@@ -0,0 +1,22 @@
+const fruits = [
+   { number: 1, title: "사과"}
+,  { number: 2, title: "배"}
+,  { number: 3, title: "바나나"}
+,  { number: 4, title: "복숭아"}
+,  { number: 5, title: "포도"}
+,  { number: 6, title: "두리안"}
+,  { number: 7, title: "코코넛"}
+,  { number: 8, title: "자두"}
+,  { number: 9, title: "앵두"}
+,  { number: 10, title: "귤"}
+]
+
+fruits [0].number + " " + fruits[0].title
+//1 사과
+
+//....
+
+
+
+
+
---
+++ Freecamp_Frontend_Study/class/03-javascript/연습-03-object.js
@@ -0,0 +1,31 @@
+let soccer = {
+    funnest : 'MAN U',
+    greatest : 'barcelona',
+    goat : 'real madrid',
+    son : 'tot'
+}
+
+soccer
+//{funnest : 'MAN U',greatest : 'barcelona',goat : 'real madrid',son : 'tot'}
+
+soccer.funnest
+// 'MAN U'
+
+let FootballClub = [
+    { name : "real madrid", goat : "ronaldo", num : "7"}
+,   { name : "barca", goat :"messi", num : "10"}
+,   { name : "ATM", goat : "grizmann", num : "7"}
+]
+
+FootballClub.length
+//3
+
+FootballClub [0].goat
+//ronaldo
+
+FootballClub [2].name
+//ATM
+
+
+
+
